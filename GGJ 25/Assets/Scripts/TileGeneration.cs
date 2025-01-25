958c100be099a7b3dd7ce4041b5c37d350b10bdd
gryllmfre <97410151+gryllmfre@users.noreply.github.com>
2025-01-25 13:51:44 -0800

    Update TileGeneration.cs

--- GGJ 25/Assets/Scripts/TileGeneration.cs
+++ GGJ 25/Assets/Scripts/TileGeneration.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -7,6 +8,7 @@ public class TileGeneration : MonoBehaviour
 {
     [SerializeField]
     private Tilemap map;
+    
     [SerializeField]
     private int tileSize;
 
@@ -14,52 +16,107 @@ public class TileGeneration : MonoBehaviour
     private Vector2Int[] directions = new Vector2Int[4];
     [SerializeField]
     private RuleTile ruleTile;
-    //private TileBase base;
+
+    [SerializeField]
+    private TileBase[] spikeCheck;
+
+    [SerializeField]
+    private Tile spikeTile;
     void Awake()
     {
-        TileClear();
         Generate();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log(RandomDir());
     }
 
     public void Generate()
     {
-        TileClear();
+        map.ClearAllTiles();
+        map.FloodFill(new Vector3Int(20, 20, 0), ruleTile);
         Vector2Int currentPos = new Vector2Int(10, 10);
-        for (int i = 0; i < 80; i++)
+        for(int i = 0; i < 80; i++)
         {
-            currentPos += RandomDir();
-            Vector3Int tempV3 = new Vector3Int(currentPos.x, currentPos.y, 0);
+
             //bigger by having each set tile as for the nearest 3
-            
-            int x = Random.Range(2, 4);
-            int y = Random.Range(2, 4);
-            for(int j = x; j >= 0; j--)
+            Vector2Int multiplier = RandomDir();
+            for(int j = 1; j <= 3; j++)
             {
-                for(int k = y; k >= 0; k--)
-                map.SetTile(new Vector3Int(currentPos.x+j, currentPos.y+k, 0), null);
+                for(int k = 1; k <= 3; k++) 
+                {
+                    map.SetTile(new Vector3Int(currentPos.x+multiplier.x+j, currentPos.y+multiplier.y+k, 0), null);
+                }
             }
-            //currentPos = new Vector2Int(currentPos.x + x, currentPos.y + y);
+            currentPos += multiplier*3;
             
         }
+
+        for(int i = -2; i <= 22; i++)
+        {
+            foreach(Vector2Int dir in directions)
+            {
+                map.SetTile(new Vector3Int(i*dir.x, i*dir.y, 0), ruleTile);
+                //creates a plus thing because the walls are all starting from the same origin. 2 of the walls need to start from the opposite corner
+            }
+        }
+        for(int x = 0; x <= 20; x++)
+        {
+            for(int y = 0; y <= 20; y++)
+            {
+                if(SpikeSpot(new Vector3Int(x, y, 0)));
+                    {
+                        GenerateSpike(new Vector3Int(x, y, 0));
+                    }
+
+            }
+        }
+
+
+
+        // for (int i = 0; i < 80; i++)
+        // {
+        //     currentPos += RandomDir();
+        //     Vector3Int tempV3 = new Vector3Int(currentPos.x, currentPos.y, 0);
+        //     //bigger by having each set tile as for the nearest 3
+            
+        //     for(int j = 0; j <= 2; j++)
+        //     {
+        //         for(int k = 0; k <= 2; k++)
+        //         {
+        //             map.SetTile(new Vector3Int(currentPos.x+j, currentPos.y+k, 0), null);
+        //         }
+
+        //     }
+        //     currentPos = new Vector2Int(currentPos.x, currentPos.y);
+            
+        // }
         //contains tile func
     }
 
 
+    private bool SpikeSpot(Vector3Int v3)
+    {
+        bool check = false;
+        bool[] tiles = new bool[4];
+        for(int i = 0; i > 2; i++)
+        {
+            Vector3Int tempv3 = new Vector3Int(v3.x+i, v3.y, 0);
+            tiles[i] = map.HasTile(tempv3);
+        }
+        for(int i = 0; i > 2; i++)
+        {
+            Vector3Int tempv3 = new Vector3Int(v3.x+1, v3.y+i, 0);
+            tiles[i] = map.HasTile(tempv3);
+        }
+        //if tiles matches a possible shape of bool[] that means we can spawn a spike, then check is true
+        return check;
+    }
     public Vector2Int RandomDir()
     {
         return directions[Random.Range(0, directions.Length)];
     }
 
-    private void TileClear()
+    public void GenerateSpike(Vector3Int v3)
     {
-        map.ClearAllTiles();
-        map.FloodFill(new Vector3Int(20, 20, 0), ruleTile);
-        
+        map.SetTile(v3, spikeTile);
+        Debug.Log("SPIKE");
     }
+
 }
