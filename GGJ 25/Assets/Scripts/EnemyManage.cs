14d617781215626c6977263a0ad86c6c4d9489d7
gryllmfre <97410151+gryllmfre@users.noreply.github.com>
2025-01-24 21:32:50 -0800

    bubble stuff

--- GGJ 25/Assets/Scripts/EnemyManage.cs
+++ GGJ 25/Assets/Scripts/EnemyManage.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements.Experimental;
 
@@ -8,6 +9,12 @@ public class EnemyManage : MonoBehaviour
 
     [SerializeField]
     int health;
+    Transform bubbleObj;
+    private void Awake()
+    {
+        bubbleObj = this.gameObject.transform.GetChild(0);
+        bubbleChange(false);
+    }
 
     public void healthUpdate(int change)
     {
@@ -19,10 +26,34 @@ public class EnemyManage : MonoBehaviour
         }
     }
 
+    public void bubbleChange(bool check)
+    {
+        if(check)
+        {
+            bubbleObj.gameObject.SetActive(true);
+        }
+        else
+        {
+            bubbleObj.gameObject.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        GameObject body = col.gameObject;
+        if(body.CompareTag("Bubble"))
+        {
+            bubbleChange(true);
+        }
+    }
+
     private void death()
     {
+        bubbleChange(false);
         Debug.Log("enemy died");
         Destroy(this.gameObject);
         
     }
+
+
 }
