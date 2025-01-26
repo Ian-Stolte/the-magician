6dcf94b128e53df28536306a1630d267b2771d8c
Ian Stolte <stolte@usc.edu>
2025-01-25 21:58:46 -0800

    Improve spike generation

--- GGJ 25/Assets/Scripts/TileGeneration.cs
+++ GGJ 25/Assets/Scripts/TileGeneration.cs
@@ -15,9 +15,6 @@ public class TileGeneration : MonoBehaviour
     private Tile spikeTile;
     private bool generatingSpikes;
     private int spikesGenerated;
-    
-    [SerializeField]
-    private int tileSize;
 
     [SerializeField]
     private Vector2Int[] directions = new Vector2Int[4];
@@ -25,15 +22,15 @@ public class TileGeneration : MonoBehaviour
     private RuleTile ruleTile;
 
     [SerializeField]
-    private int xScalar;
-
-    [SerializeField]
-    private int yScalar;
-
+    private int gridSize;
 
     [SerializeField]
     private BoolGroup[] spikeChecks;
     
+    [SerializeField] private Transform player;
+
+
+
     void Awake()
     {
         Generate();
@@ -41,68 +38,86 @@ public class TileGeneration : MonoBehaviour
 
     public void Generate()
     {
+        Random.InitState(System.Environment.TickCount);
         map.ClearAllTiles();
         spikeMap.ClearAllTiles();
-        map.FloodFill(new Vector3Int(xScalar*2, yScalar*2, 0), ruleTile);
-        Vector2Int currentPos = new Vector2Int(xScalar, yScalar);
+        map.FloodFill(new Vector3Int(gridSize*2, gridSize*2, 0), ruleTile);
+        Vector2Int currentPos = new Vector2Int(gridSize, gridSize);
         for(int i = 0; i < 80; i++)
         {
-
-            //bigger by having each set tile as for the nearest 3
-            Vector2Int multiplier = RandomDir();
-            for(int j = 1; j <= 3; j++)
+            Vector2Int multiplier = directions[Random.Range(0, directions.Length)];
+            if (currentPos.x + multiplier.x > 78 || currentPos.x + multiplier.x < 2 || currentPos.y + multiplier.y > 78 || currentPos.y + multiplier.y < 2)
             {
-                for(int k = 1; k <= 3; k++) 
+                multiplier *= -1;
+            }
+            for(int j = 1; j <= 4; j++)
+            {
+                for(int k = 1; k <= 4; k++) 
                 {
                     map.SetTile(new Vector3Int(currentPos.x+multiplier.x+j, currentPos.y+multiplier.y+k, 0), null);
                 }
             }
             currentPos += multiplier*3;
-            
         }
 
-        // for(int i = -2; i <= 22; i++)
-        // {
-        //     foreach(Vector2Int dir in directions)
-        //     {
-        //         map.SetTile(new Vector3Int(i*dir.x, i*dir.y, 0), ruleTile);
-        //         //creates a plus thing because the walls are all starting from the same origin. 2 of the walls need to start from the opposite corner
-        //     }
-        // }
-
-        for(int x = 0; x <= xScalar; x++)
+        for(int x = 0; x <= gridSize*2 -3; x++)
         {
-            for(int y = 0; y <= yScalar; y++)
+            for(int y = 0; y <= gridSize*2 -3; y++)
+            {
+                if (NumNeighbors(new Vector3Int(x, y, 0)) < 2 && map.HasTile(new Vector3Int(x, y, 0)))
+                {
+                    Debug.Log("Destroying tile at (" + x + ", " + y + ")!");
+                    map.SetTile(new Vector3Int(x, y, 0), null);
+                    x = 0;
+                    y = 0;
+                }
+                else if (NumNeighbors(new Vector3Int(x, y, 0)) == 0)
+                {
+                    player.position = new Vector3(x, y, 0);
+                }
+            }
+        }
+
+        for (int x = -10; x < gridSize*2+10; x++)
+        {
+            for (int y = -10; y < gridSize*2+10; y++)
+            {
+                if (x < 0 || x > gridSize*2 || y < 0 || y > gridSize*2)
+                    map.SetTile(new Vector3Int(x, y, 0), ruleTile);
+            }
+        }
+
+        /*for(int x = 0; x <= gridSize; x++)
+        {
+            for(int y = 0; y <= gridSize; y++)
             {
                 Vector3Int tempV3 = new Vector3Int(x, y, 0);
                 OpenSpot(tempV3);
             }
         }
         EntranceGenerate();
-        ExitGenerate();
-
-
-
-        // for (int i = 0; i < 80; i++)
-        // {
-        //     currentPos += RandomDir();
-        //     Vector3Int tempV3 = new Vector3Int(currentPos.x, currentPos.y, 0);
-        //     //bigger by having each set tile as for the nearest 3
-            
-        //     for(int j = 0; j <= 2; j++)
-        //     {
-        //         for(int k = 0; k <= 2; k++)
-        //         {
-        //             map.SetTile(new Vector3Int(currentPos.x+j, currentPos.y+k, 0), null);
-        //         }
-
-        //     }
-        //     currentPos = new Vector2Int(currentPos.x, currentPos.y);
-            
-        // }
-        //contains tile func
+        ExitGenerate();*/
     }
 
+    private int NumNeighbors(Vector3Int currentPos)
+    {
+        int neighbors = 0;
+        foreach (Vector2Int dir in directions)
+        {
+            if (map.HasTile(new Vector3Int(currentPos.x + dir.x, currentPos.y + dir.y, 0)))
+                neighbors++;
+        }
+        /*
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (map.HasTile(new Vector3Int(currentPos.x + x, currentPos.y + y, 0)) && !(x == 0 && y == 0))
+                    neighbors++;
+            }
+        }*/
+        return neighbors;
+    }
 
     private void OpenSpot(Vector3Int v3)
     {
@@ -139,10 +154,6 @@ public class TileGeneration : MonoBehaviour
     {
         bool spawned = false;
 
-    }
-    public Vector2Int RandomDir()
-    {
-        return directions[Random.Range(0, directions.Length)];
     }
 
     public void GenerateSpike(Vector3Int v3)
