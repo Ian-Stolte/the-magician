934d0071e652d727ea583c61f0ca2b4424eaad87
gryllmfre <97410151+gryllmfre@users.noreply.github.com>
2025-01-25 17:46:51 -0800

    Title Animation and Scene Change

--- GGJ 25/Assets/Scripts/FadeMessage.cs
+++ GGJ 25/Assets/Scripts/FadeMessage.cs
@@ -3,8 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class FadeMessage : MonoBehaviour
 {
+    [SerializeField]
+    private Animation fade;
     public void nextScene()
     {
         SceneManager.LoadScene("Level Scene", LoadSceneMode.Single);
+    }
+
+    public void fadePlay()
+    {
+        fade.Play();
     }
 }
